18739de8d03b68bb796a8736556d3ad63cfd8d3e
Preethika L <preethika1808@gmail.com>
2026-05-22 10:50:17 +0530

    update: added greeks

--- black_scholes.py
+++ black_scholes.py
@@ -13,7 +13,7 @@ X = 320     # change to user input
 r = 0.05    # fixed rate from fixed income 
 T = 1       # change to user input
 
-# calculate call and put prices
+# define formulas for option prices and greeks
 def compute_d1_d2(S, X, r, T, std):
     d1 = (np.log(S/X) + (r + (std**2)/2) * T) / (std * np.sqrt(T))
     d2 = d1 - std * np.sqrt(T)
@@ -22,15 +22,45 @@ def compute_d1_d2(S, X, r, T, std):
 def N(x):
     return norm.cdf(x)
 
+def n(x):
+    return norm.pdf(x)
+
 def compute_call_put(S, X, r, T, std):
     d1, d2 = compute_d1_d2(S, X, r, T, std)
     call = S * N(d1) - X * np.exp(-r*T) * N(d2)
     put = X * np.exp(-r*T) * N(-d2) - S * N(-d1)
     return call, put
 
+def compute_greeks(S,X,r,T,std):
+    d1, d2 = compute_d1_d2(S,X,r,T,std)
+    
+    delta_call = N(d1)
+    delta_put = -N(-d1)
+
+    theta_call = (-(S * n(d1) * std)/(2*np.sqrt(T))) - (r * X * (np.e**(-r*T)) * N(d2))
+    theta_put = (-(S * n(d1) * std)/(2*np.sqrt(T))) + (r * X * (np.e**(-r*T)) * N(-d2))
+
+    gamma = n(d1)/(S*std*np.sqrt(T))
+
+    vega = S*np.sqrt(T)*n(d1)
+
+    rho_call = X * T * np.e**(-r*T) * N(d2)
+    rho_put = -X * T * np.e**(-r*T) * N(-d2)
+
+    return {
+        "delta_call"    : delta_call,
+        "delta_put"     : delta_put,
+        "theta_call"    : theta_call,
+        "theta_put"     : theta_put,
+        "gamma"         : gamma,
+        "vega"          : vega,
+        "rho_call"      : rho_call,
+        "rho_put"       : rho_put,
+    }
+
+# calculate prices and verify put-call parity
 call_price, put_price = compute_call_put(S, X, r, T, std)
 
-# call-put parity check
 parity_check = S - X * np.exp(-r*T)
 if np.isclose(call_price - put_price, parity_check):
     print(f"Put-call parity holds")
@@ -38,3 +68,8 @@ else:
     print(f"Parity difference: {call_price - put_price - parity_check}")
 
 print(f"Call: {call_price:.2f}, Put: {put_price:.2f}")
+
+# calculate greeks
+greeks = pd.DataFrame(compute_greeks(S,X,r,T,std), index=["values"]).T
+greeks["values"] = greeks["values"].round(4)
+print(greeks)
